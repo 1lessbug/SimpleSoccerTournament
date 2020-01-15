b657cc52d692dc01ad04b08335f066709b9f5697
1lessbug <ashok.subedi89@gmail.com>
2020-01-14 21:05:17 -0500

    remove extra things to start in clean slate

--- app/src/main/java/ashok/subedi89/simplesoccertournament/MainActivity.kt
+++ app/src/main/java/ashok/subedi89/simplesoccertournament/MainActivity.kt
@@ -1,7 +1,7 @@
 package ashok.subedi89.simplesoccertournament
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
